6cc51f6d44453cb7248f964d51d4e1c7b1e43f9a
paulb20 <paul.bivand@blueyonder.co.uk>
2017-09-18 17:07:59 +0100

    Updates to replace geom_joy with geom_density_ridges

--- tryout_svyciprop.R
+++ tryout_svyciprop.R
@@ -5,3 +5,13 @@
 #    summarize(prop = survey_mean(levqul15 == cn, proportion = TRUE, vartype = "ci")) %>%
 #    mutate(levqul = cn)
 #})
+
+latestlfs %>% filter(!is.na(LEVQUL15)) %>% ggplot(aes(x=GRSSWK, y=to_label(LEVQUL15))) + geom_joy(rel_min_height=0.01) + labs(title="Weekly earnings of workers, AJ 2017", x="Gross weekly earnings", y="Qualification level") + theme_joy()
+latestlfs %>% filter(!is.na(HIQUL15D), GRSSWK < 1500) %>% ggplot(aes(x=GRSSWK, y=to_label(HIQUL15D))) + geom_joy(rel_min_height=0.01) + labs(title="Weekly earnings of workers, AJ 2017", x="Gross weekly earnings", y="Qualification level") + theme_joy()
+latestlfs %>% filter(!is.na(HIQUL15D), HIQUL15D != 7,GRSSWK < 1500) %>% ggplot(aes(x=GRSSWK, y=to_label(HIQUL15D))) + geom_joy(rel_min_height=0.01) + labs(title="Weekly earnings of workers, AJ 2017", x="Gross weekly earnings", y="Qualification level") + theme_joy()
+latestlfs %>% filter(!is.na(LEVQUL15), GRSSWK < 1500) %>% ggplot(aes(x=GRSSWK, y=to_label(LEVQUL15))) + geom_joy(rel_min_height=0.01) + labs(title="Weekly earnings of workers, AJ 2017", x="Gross weekly earnings", y="Qualification level") + theme_joy()
+
+
+
+
+
